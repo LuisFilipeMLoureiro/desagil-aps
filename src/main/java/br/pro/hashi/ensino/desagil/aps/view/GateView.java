c0ce012ec827f897d67295544ca0add377281e6b
Luís Filipe Loureiro <luisfml@al.insper.edu.br>
2020-04-15 18:10:27 -0300

    No meio do trabalho

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -4,19 +4,17 @@ import br.pro.hashi.ensino.desagil.aps.model.Gate;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.net.URL;
 
 public class GateView extends FixedPanel implements ActionListener, MouseListener{
     private final Gate gate;
 
-    private final JTextField weightField;
-    private final JTextField radiusField;
-    private final JTextField resultField;
-    private final Image image;
+    boolean
+
+    private final JCheckBox entrada1 = new JCheckBox("Entrada 1");
+    private final JCheckBox entrada2 = new JCheckBox("Entrada 2");
+    private final JCheckBox saida = new JCheckBox("Saída");
     // Novos atributos necessários para esta versão da interface.
     private Color color;
 
@@ -28,38 +26,31 @@ public class GateView extends FixedPanel implements ActionListener, MouseListene
 
         this.gate = gate;
 
-        weightField = new JTextField();
-        radiusField = new JTextField();
-        resultField = new JTextField();
 
-        JLabel weightLabel = new JLabel("Weight");
-        JLabel radiusLabel = new JLabel("Radius");
-        JLabel resultLabel = new JLabel("Result");
+        JLabel entradaLabel = new JLabel("Entradas:");
+        JLabel saidaLabel = new JLabel("Saída:");
+
 
         // Não há mais a chamada de setLayout, pois ela agora
         // acontece no construtor da superclasse FixedPanel.
 
         // Como subclasse de FixedPanel, agora podemos definir a
         // posição e o tamanho de cada componente ao adicioná-la.
-        add(weightLabel, 10, 10, 75, 25);
-        add(weightField, 85, 10, 150, 25);
-        add(radiusLabel, 10, 45, 75, 25);
-        add(radiusField, 85, 45, 150, 25);
-        add(resultLabel, 10, 311, 75, 25);
-        add(resultField, 85, 311, 120, 25);
+        add(entradaLabel, 10, 10, 75, 25);
+        add(entrada1, 10, 45, 150, 25);
+        add(entrada2, 10, 80, 150, 25);
+        add(saidaLabel, 10, 250, 75, 25);
+        add(saida, 10, 311, 120, 25);
 
         // Inicializamos o atributo de cor simplesmente como preto.
         color = Color.BLACK;
 
-        // Usamos esse carregamento nos Desafios, vocês lembram?
-        String name = gate.toString() + ".png";
-        URL url = getClass().getClassLoader().getResource(name);
-        image = getToolkit().getImage(url);
 
-        weightField.addActionListener(this);
-        radiusField.addActionListener(this);
 
-        resultField.setEnabled(false);
+        entrada1.addActionListener(this);
+        entrada2.addActionListener(this);
+
+        saida.setEnabled(false);
 
         // Toda componente Swing tem uma lista de observadores
         // que reagem quando algum evento de mouse acontece.
@@ -70,25 +61,31 @@ public class GateView extends FixedPanel implements ActionListener, MouseListene
         // adicionamos o "implements MouseListener" lá em cima.
         addMouseListener(this);
 
-        update();
+        // update();
     }
 
-    private void update() {
+    private void itemStateChanged(ItemEvent e) {
+
+        result = gate.
+    }
+
+    /*private void update() {
         double weight;
         double radius;
 
         try {
-            weight = Double.parseDouble(weightField.getText());
-            radius = Double.parseDouble(radiusField.getText());
+            weight = Double.parseDouble(entrada1.get());
+            radius = Double.parseDouble(entrada2.getText());
         } catch (NumberFormatException exception) {
             resultField.setText("?");
             return;
         }
 
-        double result = gate.calculate(weight, radius);
+        boolean result = gate.read();
 
         resultField.setText(Double.toString(result));
     }
+*/
 
     @Override
     public void actionPerformed(ActionEvent event) {
